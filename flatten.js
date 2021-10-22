1671706706048ab05b6a600627dd49fd1573d503
Jacky Tchen <jackiestchen@gmail.com>
2021-10-22 02:25:03 +0000

    version 0.0.0

---
+++ flatten.js
@@ -0,0 +1,65 @@
+const assertEqual = function(actual, expected) {
+  (typeof actual === 'string') ? (actual = "\"" + actual + "\"") : (actual);
+  (typeof expected === 'string') ? (expected = "\"" + expected + "\"") : (expected);
+  const failedEmoji = String.fromCodePoint(0x1F534);
+  const passedEmoji = String.fromCodePoint(0x1F7E2);
+
+  if (actual === expected) {
+    // return console.log(`${passedEmoji}${passedEmoji}${passedEmoji} Assertion Passed: ${actual} === ${expected}`);
+    return true;
+  } else {
+    // return console.log(`${failedEmoji}${failedEmoji}${failedEmoji} Assertion Failed: ${actual} !== ${expected}`);
+    return false;
+  }
+
+};
+
+// console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
+// console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
+
+
+function eqArrays (arr1, arr2) {
+  if (arr1.length !== arr2.length){
+    return false;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (!assertEqual(arr1[i], arr2[i])){
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+
+function assertArrayEqual(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    return console.log(`🟢🟢🟢 Assertion Passed: ${arr1} !== ${arr2}`);
+  } else {
+    return console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+}
+
+// assertArrayEqual([1,2,3], [1,2,3]);
+// assertArrayEqual([1,2,3], [3,2,1]);
+// assertArrayEqual(['1','2','3'], ['1','2','3']);
+// assertArrayEqual(['1','2','3'], ['1','2',3]);
+
+
+
+function flatten (arr) {
+  let output = [];
+  arr.forEach(function (element) {
+    if (Array.isArray(element)) {
+      output.push(...flatten(element));
+    } else {
+      output.push(element);
+    }
+  })
+  return output;
+}
+
+console.log(flatten([1,2,[3,4],5,[6]]));
