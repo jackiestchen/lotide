e9d702c5d50108150114b889a3e971916ef25de0
Jacky Tchen <jackiestchen@gmail.com>
2021-10-23 23:25:10 +0000

    version 0.0.0

--- map.js
+++ map.js
@@ -1,16 +1,4 @@
-const assertEqual = function(actual, expected) {
-  (typeof actual === 'string') ? (actual = "\"" + actual + "\"") : (actual);
-  (typeof expected === 'string') ? (expected = "\"" + expected + "\"") : (expected);
-  const failedEmoji = String.fromCodePoint(0x1F534);
-  const passedEmoji = String.fromCodePoint(0x1F7E2);
 
-  if (actual === expected) {
-    return console.log(`${passedEmoji}${passedEmoji}${passedEmoji} Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    return console.log(`${failedEmoji}${failedEmoji}${failedEmoji} Assertion Failed: ${actual} !== ${expected}`);
-  }
-
-};
 
 
 
