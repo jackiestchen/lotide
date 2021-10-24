d03a64fa94c661d837b53c05927958ac95a0a49b
Jacky Tchen <jackiestchen@gmail.com>
2021-10-24 01:00:56 +0000

    version 0.0.0

---
+++ findKey.js
@@ -0,0 +1,27 @@
+const findKey = function (input, callback) {
+  let result; 
+
+  for (const el in input ){
+    // console.log(el);
+    // console.log(callback(input[el]));
+    // console.log(input[el].stars);
+    let booleanTest = callback(input[el]);
+    if (booleanTest) {
+      result = el;
+      break;
+    }
+  }
+  return result;
+}
+
+
+const cards = {
+  "Blue Hill":  {stars: 1},
+  "Akaleri":    {stars: 3},
+  "noma":       {stars: 2},
+  "elBulli":    {stars: 3},
+  "Ora":        {stars:2},
+  "Akelarre":   {stars: 3}
+}
+
+console.log(findKey(cards, x => x.stars === 2)); // => noma
