22f83e947a21329d8f064ce796cd62cc617294da
jackiestchen <jackiestchen@gmail.com>
2021-11-01 21:16:47 -0600

    "Implement recursion"

--- eqArrays.js
+++ eqArrays.js
@@ -23,16 +23,23 @@
 const assertEqual = require("./assertEqual");
 
 function eqArrays (arr1, arr2) {
-  if (arr1.length !== arr2.length){
-    return false;
-  } else {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]){
-        return false;
-      }
+  if (arr1.length !== arr2.length) return false;
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (Array.isArray(arr1[i])) {
+      if (!eqArrays(arr1[i], arr2[i])) return false;
+    } else {
+      if (arr1[i] !== arr2[i]) return false;
     }
-    return true;
   }
+
+  return true;
+ 
 }
 
 module.exports = eqArrays;
+
+
+// console.log(eqArrays([[2, 3], [4]], [[2, 3], [4]])); // => true
+// console.log(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]])); // => false
+// console.log(eqArrays([[2, 3], [4]], [[2, 3], 4])); // => false
